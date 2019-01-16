007df1c006c456b536154db4b97613b4461c9359
kenbasco <kenneth.basco@rutgers.edu>
2019-01-16 12:41:29 -0500

    added sections and some content

--- canvas.js
+++ canvas.js
@@ -9,7 +9,7 @@ fitToContainer(canvas);
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width ='100%';
-  canvas.style.height='100%';
+  canvas.style.height='70vh';
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
