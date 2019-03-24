e3a5cc39ad4b06fc9cf406b7ee2221861da67c97
kennethbasco <kpb82@scarletmail.rutgers.edu>
2019-03-24 16:27:16 -0400

    decreased canvas size, changed p style properties
    - canvas side 100%-->70%
    - changed text color to white
    - changed color of links
    - changed ratio of footer and contact sections to the screen

--- canvas.js
+++ canvas.js
@@ -8,8 +8,8 @@ fitToContainer(canvas);
 
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
-  canvas.style.width ='100%';
-  canvas.style.height='100%';
+  canvas.style.width ='70%';
+  canvas.style.height='70%';
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
